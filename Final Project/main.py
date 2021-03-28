cc9bb78e990d8f6f5275b3d573d0d9426c04c182
Jadesola Bejide <59064406+jade-bejide@users.noreply.github.com>
2021-03-28 17:15:18 +0100

    Delete Final Project directory

--- Final Project/main.py
+++
@@ -1,9 +0,0 @@
-from queue_class import *
-from titles_class import *
-from user_class import *
-from recommender_system import *
-from login_functions import *
-#from windows import *
-from guizero import *
-
-LoginScreen()
--- Final Project/windows.py
+++
@@ -1,6 +0,0 @@
-from guizero import *
-
-
-
-
-ShowLoginScreen()
