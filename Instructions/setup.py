42388b4f983086a4b7e1c02336b1308c245def52
Jadesola Bejide <59064406+jade-bejide@users.noreply.github.com>
2021-04-01 16:20:37 +0100

    Add files via upload
    Includes images in set up

--- Instructions/setup.py
+++ Instructions/setup.py
@@ -4,12 +4,16 @@ base = None
 
 executables = [Executable("rate_it.py", base=base)]
 
+includefiles = ["leftarrow.png", "rightarrow.png", "staricon.png"]
+includes = []
 packages = ["idna", "guizero", "mysql.connector", "json", "urllib", "numpy",
             "math", "requests", "random", "PIL", "sys", "tkinter"]
 
 options = {
     'build_exe': {
         'packages':packages,
+        'includes': includes,
+        'include_files': includefiles
         },
 }
 
@@ -17,6 +21,7 @@ setup(
     name = "<Social Recommender System>",
     options = options,
     version = "0.0.1",
+    author = "Jadesola Bejide",
     description = "Rate movies and TV shows and follow other users to get recommendations",
     executables = executables
 )
